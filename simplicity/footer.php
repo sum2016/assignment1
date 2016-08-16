b0e21ccc491cc1d537c0ef13ecb78ba567d3fd08
sum2016 <Lin@Helens-MacBook-Pro.local>
2016-08-16 19:14:36 -0400

    change in footer
    change in footer
    
    added copyright line

--- simplicity/footer.php
+++ simplicity/footer.php
@@ -17,9 +17,13 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'simplicity' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'simplicity' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'simplicity' ), 'simplicity', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'simplicity' ), 'simplicity', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' . ' edited by Helen' ); ?>
 		</div><!-- .site-info -->
 		
+		<div class="site-info">
+				<p class="copyright">&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url(); ?>"><img src="<?php header_image(); ?>" alt="webiste logo"></a>. All Rights Reserved.</p> 
+		</div><!-- .site-info -->
+
 		<div id="footer-menu">
 			<?php wp_nav_menu( array( 'theme_location'=>'menufoot', 'menu_class'=>'foot-menu')); ?>
 		</div><!-- #footer-menu -->
