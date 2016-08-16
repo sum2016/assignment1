9a25c19c1c6c89a8a74fb5e96ef022abe27f04d4
sum2016 <Lin@Helens-MacBook-Pro.local>
2016-08-16 19:00:39 -0400

    options v.2
    options v.2
    change

--- simplicity/inc/options.php
+++ simplicity/inc/options.php
@@ -13,9 +13,10 @@ function add_submenu() {
 add_action( 'admin_menu', 'add_submenu' );
 	
 
-function settings_init() { 
+function settings_new() { 
 	register_setting( 'theme_options', 'options_settings' );
 	
+	//Navigation Setting
 	add_settings_section(
 		'options_page_section', 
 		'Navigation', 
@@ -25,42 +26,40 @@ function settings_init() {
 	function options_page_section_callback() { 
 		echo 'Customize navigation using options below.';
 	}
-
-	//On/Off Navigation
-	add_settings_field( 
-		'select_field', 
-		'Turn Navgation On/Off', 
-		'select_field_render', 
-		'theme_options', 
-		'options_page_section'  
-	);	
-	function select_field_render() { 
-		$options = get_option( 'options_settings' );
-		?>
-		<select name="options_settings[select_field]">
-			<option value="1" <?php if (isset($options['select_field'])) selected( $options['select_field'], 1 ); ?>>On</option>
-			<option value="2" <?php if (isset($options['select_field'])) selected( $options['select_field'], 2 ); ?>>Off</option>
-		</select>
-	<?php
-	}
-
-	//Location of Navigation
+		//On/Off Navigation
 		add_settings_field( 
-		'radio_field', 
-		'Choose the Location of the Navigation Bar', 
-		'radio_field_render', 
-		'theme_options', 
-		'options_page_section'
-	);
-	function radio_field_render() { 
-		$options = get_option( 'options_settings' );
-		?>
-		<input type="radio" name="options_settings[radio_field]" <?php if (isset($options['radio_field'])) checked( $options['radio_field'], 1 ); ?> value="1" /> <label>Right</label><br />
-		<input type="radio" name="options_settings[radio_field]" <?php if (isset($options['radio_field'])) checked( $options['radio_field'], 2 ); ?> value="2" /> <label>Left</label>
+			'select_field', 
+			'Turn Navgation On/Off', 
+			'select_field_render', 
+			'theme_options', 
+			'options_page_section'  
+		);	
+		function select_field_render() { 
+			$options = get_option( 'options_settings' );
+			?>
+			<select name="options_settings[select_field]">
+				<option value="1" <?php if (isset($options['select_field'])) selected( $options['select_field'], 1 ); ?>>On</option>
+				<option value="2" <?php if (isset($options['select_field'])) selected( $options['select_field'], 2 ); ?>>Off</option>
+			</select>
 		<?php
-	}
+		}
+		//Location of Navigation
+			add_settings_field( 
+			'radio_field', 
+			'Choose the Location of the Navigation Bar', 
+			'radio_field_render', 
+			'theme_options', 
+			'options_page_section'
+		);
+		function radio_field_render() { 
+			$options = get_option( 'options_settings' );
+			?>
+			<input type="radio" name="options_settings[radio_field]" <?php if (isset($options['radio_field'])) checked( $options['radio_field'], 1 ); ?> value="1" /> <label>Right</label><br />
+			<input type="radio" name="options_settings[radio_field]" <?php if (isset($options['radio_field'])) checked( $options['radio_field'], 2 ); ?> value="2" /> <label>Left</label>
+			<?php
+		}
 
-	//////
+	//Copyright Setting
 	add_settings_section(
 		'options_page_section_nav', 
 		'Copyright', 
@@ -70,41 +69,40 @@ function settings_init() {
 	function options_page_section_callback_nav() { 
 		echo 'Customize copyright line using option below.';
 	}
-	//Copyright
-		add_settings_field( 
-		'checkbox_field', 
-		'Check your preference', 
-		'checkbox_field_render', 
-		'theme_options', 
-		'options_page_section_nav'  
-	);
-	function checkbox_field_render() { 
-		$options = get_option( 'options_settings' );
-	?>
-		<input type="checkbox" name="options_settings[checkbox_field]" <?php if (isset($options['checkbox_field'])) checked( 'on', ($options['checkbox_field']) ) ; ?> value="on" />
-		<label>Turn it On</label> 
-		<?php	
-	}
-
-	add_settings_field( 
-		'text_field', 
-		'Enter New Copyright Line', 
-		'text_field_render', 
-		'theme_options', 
-		'options_page_section_nav' 
-	);
-	function text_field_render() { 
-		$options = get_option( 'options_settings' );
+		//Change Copyright Line
+			add_settings_field( 
+			'checkbox_field', 
+			'Check your preference', 
+			'checkbox_field_render', 
+			'theme_options', 
+			'options_page_section_nav'  
+		);
+		function checkbox_field_render() { 
+			$options = get_option( 'options_settings' );
 		?>
-		<input type="text" name="options_settings[text_field]" placeholder="enter copyright line" value="<?php if (isset($options['text_field'])) echo $options['text_field']; ?>" />
-		<?php
-	}
+			<input type="checkbox" name="options_settings[checkbox_field]" <?php if (isset($options['checkbox_field'])) checked( 'on', ($options['checkbox_field']) ) ; ?> value="on" />
+			<label>Turn it On</label> 
+			<?php	
+		}
 
+		add_settings_field( 
+			'text_field', 
+			'Enter New Copyright Line', 
+			'text_field_render', 
+			'theme_options', 
+			'options_page_section_nav' 
+		);
+		function text_field_render() { 
+			$options = get_option( 'options_settings' );
+			?>
+			<input type="text" name="options_settings[text_field]" placeholder="enter copyright line" value="<?php if (isset($options['text_field'])) echo $options['text_field']; ?>" />
+			<?php
+		}
 
 	function my_theme_options_page(){ 
 		?>
 		<form action="options.php" method="post">
-			<h2>Customize Setting</h2>
+			<h1>Customize Setting</h1>
 			<?php
 			settings_fields( 'theme_options' );
 			do_settings_sections( 'theme_options' );
@@ -113,7 +111,6 @@ function settings_init() {
 		</form>
 		<?php
 	}
-
 }
 
-add_action( 'admin_init', 'settings_init' );
+add_action( 'admin_init', 'settings_new' );
