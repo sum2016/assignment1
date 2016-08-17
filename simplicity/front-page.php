132123c8075e9a38739da1c63856e262697532c4
sum2016 <Lin@Helens-MacBook-Pro.local>
2016-08-17 00:24:49 -0400

    front-page original
    front-page original

---
+++ simplicity/front-page.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The template for displaying front page.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package simplicity
+ */
+
+get_header();
+get_sidebar();
+get_footer();
