048c763117874c20720aa0af541353b087b0ca4f
sum2016 <Lin@Helens-MacBook-Pro.local>
2016-08-18 03:51:44 -0400

    custom post type v.2
    custom post type v.2
    changes in display and navigation

--- simplicity/front-page.php
+++ simplicity/front-page.php
@@ -14,15 +14,35 @@ get_header();
 	<main id="main" class="site-main" role="main">
 
 		<?php
-			$portfolio = array( 'post_type' => 'Portfolio', 'posts_per_page' => 4 );
+		if (have_posts()) :
+			if (is_front_page() && ! is_home()) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+
+			<?php
+			endif;
+			$portfolio = array( 'post_type' => 'Portfolio', 'posts_per_page' => 1 );
 			$query = new WP_Query( $portfolio );
 				while ( $query->have_posts() ) : $query->the_post();
-get_template_part( 'template-parts/content', get_post_format() );
+					get_template_part( 'template-parts/content', get_post_format() );
 
-
+					echo 'Advertisement Link: ' . get_post_meta(get_the_ID(), 'link_key', true);
+				
 				endwhile;
-		the_posts_navigation();
 
+		the_posts_navigation(
+			array(
+			'mid_size' => 2,
+			'prev_text' => __('&larr; Older Anlysis', '&larr; Older Anlysis'),
+			'next_text'=> __('Recent Anlysis &rarr;', 'Recent Anlysis &rarr;')
+			)
+		);
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
 		?>
 
 	</main><!-- #main -->
